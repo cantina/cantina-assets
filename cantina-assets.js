b72fa594ede386216b3b357d5e5fdeae4e4e4dfa
Jesse Street <jahjahdisciple@gmail.com>
2013-08-09 15:57:31 -0700

    CSS namespaces. WIP.

--- cantina-assets.js
+++ cantina-assets.js
@@ -1,9 +1,13 @@
-var app = require('cantina')
-  , async = require('async');
+var app = require('cantina');
 
-if (app.conf.get('optimize:status') === 'enabled') {
-  app.optimize = {};
-  app.hook('start').add(-500, require('./lib/css.js'));
+app.assets = module.exports = {
+  css: require('./lib/css.js'),
+  js: require('./lib/js.js'),
+  templates: require('./lib/templates.js')
+};
+
+if (app.conf.get('assets:optimize') === 'enabled') {
+  app.hook('start').add(-500, app.assets.css.optimize);
   app.hook('start').add(-500, require('./lib/js.js'));
   app.hook('start').add(-500, require('./lib/templates.js'));
 }
