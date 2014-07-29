130ec2e66ac4d3eef551cc850e83378ed9b4692f
Brian Link <cpsubrian@gmail.com>
2014-07-28 20:18:56 -0700

    Updates to cantina 4.1.x

--- cantina-assets.js
+++ cantina-assets.js
@@ -1,12 +1,12 @@
-var app = require('cantina');
+module.exports = function (app) {
+  app.assets = module.exports = {};
+  app.require('./lib/css.js');
+  app.require('./lib/js.js');
+  app.require('./lib/templates.js');
 
-app.assets = module.exports = {};
-require('./lib/css.js');
-require('./lib/js.js');
-require('./lib/templates.js');
-
-if (app.conf.get('assets:optimize') === 'enabled') {
-  app.hook('start').add(-503, app.assets.css.optimize);
-  app.hook('start').add(-502, app.assets.js.optimize);
-  app.hook('start').add(-501, app.assets.templates.optimize);
-}
+  if (app.conf.get('assets:optimize') === 'enabled') {
+    app.hook('start').add(-503, app.assets.css.optimize);
+    app.hook('start').add(-502, app.assets.js.optimize);
+    app.hook('start').add(-501, app.assets.templates.optimize);
+  }
+};
