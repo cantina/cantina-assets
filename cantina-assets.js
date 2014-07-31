4a025e6e0143a04d93bf2ddf3abf27c775256c76
Brian Link <cpsubrian@gmail.com>
2014-07-30 19:29:06 -0700

    Attempted fix at some export/context problems.

--- cantina-assets.js
+++ cantina-assets.js
@@ -1,12 +1,15 @@
 module.exports = function (app) {
-  app.assets = module.exports = {};
-  app.require('./lib/css.js');
-  app.require('./lib/js.js');
-  app.require('./lib/templates.js');
+  app.assets = {};
+
+  var css = app.require('./lib/css.js')
+    , js = app.require('./lib/js.js')
+    , tempaltes = app.require('./lib/templates.js');
 
   if (app.conf.get('assets:optimize') === 'enabled') {
-    app.hook('start').add(-503, app.assets.css.optimize);
-    app.hook('start').add(-502, app.assets.js.optimize);
-    app.hook('start').add(-501, app.assets.templates.optimize);
+    app.hook('start').add(-503, css.optimize);
+    app.hook('start').add(-502, js.optimize);
+    app.hook('start').add(-501, templates.optimize);
   }
+
+  return app.assets;
 };
